4d3b83fc7942ffe6da711c9518f23249d35db2ae
Atle Holm <atle@team-holm.net>
2015-06-30 09:05:11 +0200

    Fixing gitignore

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    Hostname: "nodenet.homelinux.net",
+    Port: 3010,
+    MongoServer: "127.0.0.1",
+    MongoPort: 27017,
+    Username: 'username',
+	Password: 'my-p@ssw0rd',
+	Database: 'diskreporter-cache-db',
+	Collection: 'DiskReporterCache',
+	Compression: false,
+    Debug: true,
+    VerboseDebug: true
+}
